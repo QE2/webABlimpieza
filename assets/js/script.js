be4efa1d98efdcda28b2c791150bf2ee0ad83f47
Javier Rodríguez <javier@qe2computing.com>
2024-12-08 13:39:46 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 
 
-
+/* Menú efecto acordeón, FAQ home */
 var col = document.getElementsByClassName("colapsable");
 var i;
 
@@ -14,4 +14,16 @@ for (i = 0; i < col.length; i++) {
       content.style.maxHeight = content.scrollHeight + "px";
     }
   });
+}
+
+/* Menú de navegación principal, colapsable en pantallas pequeñas.
+   Fuente: https://www.w3schools.com/howto/howto_js_responsive_navbar_dropdown.asp.
+   Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("cabecera-nav-pral");
+  if (x.className === "cabecera-nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "cabecera-nav";
+  }
 }
